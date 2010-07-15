88c0542f9d639fc9375a7d70ee4898f6fac79b70
Enrique Vidal <enrique@cloverinteractive.com>
2010-07-15 11:20:27 -0700

    Fixed pagination to not be shown when the content does not have more than 1 page

--- easy_slider.development.js
+++ easy_slider.development.js
@@ -1,19 +1,23 @@
 $.fn.easy_slider = function(obj, selector) {
-	if (!obj)				obj				= { page: 1, per_page: 5, nextText: 'Next', backText: 'Back', firstText: 'First', lastText: 'last', indicator: true, indicatorText: 'page [n] of [m]' }
+	if (!obj)				obj				= { page: 1, per_page: 5, nextText: 'Next', backText: 'Back', firstText: 'First', lastText: 'last', indicatorText: 'page [n] of [m]' }
 	if (!selector)	selector 	= '.paggeable';
 	
 	// Fix values in case client do not send them
-	obj.page						=	( obj.page			=== undefined )			? 1									: obj.page;
-	obj.per_page				=	( obj.per_page 	== undefined  )			? 5									: obj.per_page;
-	obj.nextText 				= ( obj.nextText 	== undefined  ) 		? 'Next' 						: obj.nextText;
-	obj.backText 				= ( obj.backText 	== undefined  ) 		? 'Back' 						: obj.backText;
-	obj.firstText				= ( obj.firstText == undefined  ) 		? 'First'						: obj.firstText;
-	obj.lastText				= ( obj.lastText	== undefined  ) 		? 'Last'						: obj.lastText;
-	obj.indicatorText		= ( obj.indicatorText == undefined )	? 'page [n] of [m]'	: obj.indicatorText;	
-	obj.indicator				= ( obj.indicator == undefined ) 			? true							: false;
+	obj.page				= ( obj.page			=== undefined )		? 1					: obj.page;
+	obj.per_page			= ( obj.per_page 	== undefined  )			? 5					: obj.per_page;
+	obj.nextText 			= ( obj.nextText 	== undefined  ) 		? 'Next' 			: obj.nextText;
+	obj.backText 			= ( obj.backText 	== undefined  ) 		? 'Back' 			: obj.backText;
+	obj.firstText			= ( obj.firstText == undefined  ) 			? 'First'			: obj.firstText;
+	obj.lastText			= ( obj.lastText	== undefined  ) 		? 'Last'			: obj.lastText;
+	obj.indicatorText		= ( obj.indicatorText == undefined )		? 'page [n] of [m]'	: obj.indicatorText;	
+	obj.indicator			= ( obj.indicator == undefined ) 			? true				: obj.indicator;
 	
 	var total_number_records 	= $(selector).length;
-	var more_pages 						= null;
+	var more_pages 				= null;
+	
+	// We get the total number of pages
+	var pages = ( total_number_records / obj.per_page );
+	pages = ( isFloat(pages) ) ? parseInt(pages) + 1 : pages;
 	
 	$("#easy_slider_move_forward").remove();
 	$("#easy_slider_move_backward").remove();
@@ -22,6 +26,7 @@ $.fn.easy_slider = function(obj, selector) {
 	$("#current_page").remove();
 
 	$(selector).hide();
+	
 	$(selector).each(function(index, value) {
 		/* 
 			When paginating the algorithm used is:
@@ -35,66 +40,63 @@ $.fn.easy_slider = function(obj, selector) {
 			more_pages = ( index == total_number_records - 1 ) ? false : true;
 		}
 	});
-	
-	// We get the total number of pages
-	pages = ( total_number_records / obj.per_page );
-	pages = ( isFloat(pages) ) ? parseInt(pages) + 1 : pages;
 
 	// Add pagination links to the plugin
-	if (more_pages)
-	{
-		move({ cssId: 					'easy_slider_move_forward', 
-						href: 					'move_forward', 
-						text: 					obj.nextText, 
-						selector: 			selector, 
-						page: 					( obj.page + 1 ), 
-						per_page: 			obj.per_page, 
-						cssClass: 			'slide',
-						indicatorText:  obj.indicatorText });
-						
-		if ( obj.indicator ) current_page(selector, obj.page, pages, obj.indicatorText);
-		
-		if ( obj.page == 1 )
-		{			
-			move({ 	cssId: 					'easy_slider_move_last', 
-							href: 					'move_last', 
-							text: 					obj.lastText, 
+	if ( pages > 1 )
+		if (more_pages)
+		{
+			move({ cssId: 					'easy_slider_move_forward', 
+							href: 					'move_forward', 
+							text: 					obj.nextText, 
 							selector: 			selector, 
-							page: 					pages, 
-							per_page: 			obj.per_page, 
-							cssClass: 			'slide',
-							indicatorText:  obj.indicatorText  });
-		}
-		else
-			move({ cssId: 					'easy_slider_move_backward', 
-							href: 					'move_backward', 
-							text: 					obj.backText, 
-							selector: 			selector, 
-							page: 					( obj.page - 1), 
+							page: 					( obj.page + 1 ), 
 							per_page: 			obj.per_page, 
 							cssClass: 			'slide',
 							indicatorText:  obj.indicatorText });
-	}	else {
-		move({ cssId: 					'easy_slider_move_first', 
-						href: 					'move_first', 
-						text: 					obj.firstText, 
-						selector: 			selector, 
-						page: 					1, 
-						per_page: 			obj.per_page, 
-						cssClass: 			'slide',
-	 					indicatorText:  obj.indicatorText });
-						
-		if ( obj.indicator ) current_page(selector, obj.page, pages, obj.indicatorText);
-						
-		move({ cssId: 					'easy_slider_move_backward', 
-						href: 					'move_backward', 
-						text: 					obj.backText, 
-						selector: 			selector, 
-						page: 					( obj.page - 1 ), 
-						per_page: 			obj.per_page, 
-						cssClass: 			'slide',
-	 					indicatorText:  obj.indicatorText });
-	}
+							
+			if ( obj.indicator ) current_page(selector, obj.page, pages, obj.indicatorText);
+			
+			if ( obj.page == 1 )
+			{			
+				move({ 	cssId: 					'easy_slider_move_last', 
+								href: 			'move_last', 
+								text: 			obj.lastText, 
+								selector: 		selector, 
+								page: 			pages, 
+								per_page: 		obj.per_page, 
+								cssClass: 		'slide',
+								indicatorText:  obj.indicatorText  });
+			}
+			else
+				move({ cssId: 					'easy_slider_move_backward', 
+								href: 			'move_backward', 
+								text: 			obj.backText, 
+								selector: 		elector, 
+								page: 			( obj.page - 1), 
+								per_page: 		obj.per_page, 
+								cssClass: 		'slide',
+								indicatorText:  obj.indicatorText });
+		}	else {
+			move({ cssId: 					'easy_slider_move_first', 
+							href:			'move_first', 
+							text: 			obj.firstText, 
+							selector: 		selector, 
+							page: 			1, 
+							per_page: 		obj.per_page, 
+							cssClass: 		'slide',
+							indicatorText:  obj.indicatorText });
+							
+			if ( obj.indicator ) current_page(selector, obj.page, pages, obj.indicatorText);
+							
+			move({ cssId: 					'easy_slider_move_backward', 
+							href: 			'move_backward', 
+							text: 			obj.backText, 
+							selector: 		selector, 
+							page: 			( obj.page - 1 ), 
+							per_page: 		obj.per_page, 
+							cssClass: 		'slide',
+							indicatorText:  obj.indicatorText });
+		}
 }
 
 // Creates the current page legend
